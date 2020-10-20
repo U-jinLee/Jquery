1dedf29ce4f0633b181ba2efc69e8920c1042467
Yoojin_Lee <71121964+leeujin1029@users.noreply.github.com>
2020-10-20 18:12:10 +0900

    Create 1_JqueryBasic.js

---
+++ 101_selector/js/1_JqueryBasic.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    /*자식 선택자:선택자>선택자 */
+    $(".container>h1").css("color", "red");/*container class의 h1 태그*/
+    /*후손 선택자:선택자 선택자 */
+    $(".container ul").css("color","pink")
+})
