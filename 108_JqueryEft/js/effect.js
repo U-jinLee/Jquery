b34b4f5e3d76f5fd7a820b57ed4637e21410c4fe
Yoojin Lee <yoojinLee.dev@gmail.com>
2020-10-23 10:42:37 +0900

    Create effect.js

---
+++ 108_JqueryEft/js/effect.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    /*effect about show, hide and toggle*/
+    $("#default").on("click",function(){
+        $("p").hide("2000","linear");/*From the front duration, easing, callback parameter is option */
+    })
+    $("#default").on("mouseleave",function(){
+        $("p").show("2000");
+    })
+    /* */
+    $("h1").on("click",function(){
+        $("p").toggle("2000");
+    })
+    $("#slideDown").on("click",function(){
+        $("p").slideDown();
+    })
+    $("#slideUp").on("click",function(){
+        $("p").slideUp();
+    })
+    $("#fadeIn").on("click",function(){
+        $("p").fadeIn("slow");
+    })
+    $("#fadeOut").on("click",function(){
+        $("p").fadeOut("slow");
+    })
+    $("#fadeToggle").on("click",function(){
+        $("p").fadeToggle("slow");
+    })
+})
