1d53e2db63eb89f6fed25f1096ece3cfa0eccc32
Yoojin Lee <yoojinLee.dev@gmail.com>
2020-10-26 10:23:25 +0900

    HelloWorld

--- 109_plugIn/js/easing.js
+++ 109_plugIn/js/easing.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-    
+    $("#easeInElastic").click(function(){
+        $("div").animate({
+            left:400
+        },2000,"easeInElastic").animate({left:0},1000,"easeInElastic")
+    })
+
+    $("#easeOutElastic").click(function(){
+        $("div").animate({
+            left:400
+        },2000,"easeOutElastic").animate({left:0},1000,"easeOutElastic")
+    })
+
+
 })
